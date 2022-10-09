7d8700f658e0eb9c8d8ebe2bc8a5729bacc27934
Nick Suchecki <nicksuchecki@gmail.com>
2022-10-09 15:59:59 -0400

    Fix subtle travis-ci and travis-pro bug

--- test/CoverallsTest.hs
+++ test/CoverallsTest.hs
@@ -5,7 +5,7 @@ import qualified Data.Text                          as T
 import           System.Environment                 (setEnv, unsetEnv)
 import           Test.Coverage.Coveralls.CIProvider
 import           Test.Tasty                         (TestName, TestTree,
-                                                     testGroup)
+                                                     testGroup, withResource)
 import           Test.Tasty.HUnit                   (testCase, (@?=))
 
 tests :: TestTree
@@ -13,13 +13,15 @@ tests = testGroup "Coveralls Testing" [travisCITest, travisProTest, jenkinsCITes
 
 travisCITest, travisProTest, jenkinsCITest :: TestTree
 travisCITest = providerTest travisCI
-travisProTest = providerTest travisPro
+-- Travis Pro will have TRAVIS set in it's env which can cause subtle bugs if we don't order our checks properly
+travisProTest = withResource (setEnv "TRAVIS" "true") (const (unsetEnv "TRAVIS")) (const (providerTest travisPro))
 jenkinsCITest = providerTest jenkinsCI
 
 providerTest :: CIProvider -> TestTree
 providerTest ci = testGroup (T.unpack (serviceName ci)) [
   testCIEnv "All values set" (Just (envFlag ci)) (Just (envServiceNumber ci)) (envServiceJobID ci) (Just (serviceName ci))
-  , testCIEnv "No Flag Set" Nothing (Just (envServiceNumber ci)) (envServiceJobID ci) Nothing
+  -- Travis Pro will default to Travis CI when not set so we put a dumb hack here
+  , testCIEnv "No Flag Set" Nothing (Just (envServiceNumber ci)) (envServiceJobID ci) (if serviceName ci == serviceName travisPro then Just (serviceName travisCI) else Nothing)
   , testCIEnv "No Number Set" (Just (envFlag ci)) Nothing (envServiceJobID ci) Nothing
   , testCIEnv "No Job ID Set" (Just (envFlag ci)) (Just (envServiceNumber ci)) Nothing (Just (serviceName ci))
   , testCIEnv "No Flag and No Number Set" Nothing Nothing (envServiceJobID ci) Nothing
@@ -28,11 +30,11 @@ providerTest ci = testGroup (T.unpack (serviceName ci)) [
 
 testCIEnv :: TestName -> Maybe String -> Maybe String -> Maybe String -> Maybe Text -> TestTree
 testCIEnv tn flag number mJobId expected = testCase tn $ do
-  actual <- withEnv flag number mJobId
+  actual <- withBuildEnv flag number mJobId
   actual @?= expected
 
-withEnv :: Maybe String -> Maybe String -> Maybe String -> IO (Maybe Text)
-withEnv flag number mJobId = do
+withBuildEnv :: Maybe String -> Maybe String -> Maybe String -> IO (Maybe Text)
+withBuildEnv flag number mJobId = do
   setEnvIf flag "ON"
   setEnvIf number "1234"
   setEnvIf mJobId "1234.1"
@@ -41,7 +43,6 @@ withEnv flag number mJobId = do
   unsetEnvIf number
   unsetEnvIf mJobId
   pure (fst3 <$> serviceData)
-
 
 unsetEnvIf :: Maybe String -> IO ()
 unsetEnvIf Nothing    = pure ()
